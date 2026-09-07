480e5c3233506189030b41e590be2b667146c63a
Umar <olowonyoumar@gmail.com>
2026-09-07 16:53:40 +0100

    I have been able to get the problem number and title from thr DOM tree

--- scripts/get_solved_question.js
+++ scripts/get_solved_question.js
@@ -1,5 +1,20 @@
 let submit_btn = document.querySelector("button[data-e2e-locator='console-submit-button']")
 
+const currentUrl = window.location.href;
+
+let description_tab = document.querySelector(".flexlayout__layout")
+
+description_tab = Array.from(description_tab.children)
+
+description_tab = (description_tab[3])
+
+description_tab = (Array.from(((Array.from(description_tab.getElementsByTagName("div")))[0]).getElementsByTagName("div")))[0]
+
+let problem_number_and_name = Array.from(((Array.from(description_tab.getElementsByTagName("div")))[0]).getElementsByTagName("a"))[0]
+
+console.log(problem_number_and_name.textContent)
+
+
 /*
 If Rejected
     "Rejected" will be in a h3 with text content of Wrong Answer
@@ -12,7 +27,7 @@ If Accepted
 function check_if_solution_is_accepted_or_rejeted(timeout = 10000) {
     return new Promise((resolve, reject) => {
         const startTime = Date.now();
-        console.log("PROMISE DEFINED!!!")
+        // console.log("PROMISE DEFINED!!!")
 
         const checkResult = () => {
 
@@ -24,7 +39,7 @@ function check_if_solution_is_accepted_or_rejeted(timeout = 10000) {
 
             if (accepted) {
                 observer.disconnect();
-                console.log("OBSERVER HAS BEEN DISCONNECTED. CORRECT ANSWERRRR")
+                // console.log("OBSERVER HAS BEEN DISCONNECTED. CORRECT ANSWERRRR")
 
                 resolve({
                     type: "Accepted",
@@ -35,7 +50,7 @@ function check_if_solution_is_accepted_or_rejeted(timeout = 10000) {
 
             if (wrong_answer) {
                 observer.disconnect();
-                console.log("OBSERVER HAS BEEN DISCONNECTED. WRONG ANSWERRRR")
+                // console.log("OBSERVER HAS BEEN DISCONNECTED. WRONG ANSWERRRR")
 
                 resolve({
                     type: "wrong_answer",
@@ -46,7 +61,7 @@ function check_if_solution_is_accepted_or_rejeted(timeout = 10000) {
 
             if (Date.now() - startTime >= timeout) {
                 observer.disconnect();
-                console.log("OBSERVER HAS BEEN DISCONNECTED. TIME EXCEEDED")
+                // console.log("OBSERVER HAS BEEN DISCONNECTED. TIME EXCEEDED")
 
                 reject(new Error("Timed out waiting for result"));
             }
@@ -66,20 +81,37 @@ function check_if_solution_is_accepted_or_rejeted(timeout = 10000) {
 
 
 if (submit_btn){
-    console.log(submit_btn)
+    // console.log(submit_btn)
 
 
 
 
     submit_btn.addEventListener("click", (event) => {
-        console.log("SUBMIT BUTTON CLICKED")
+        // console.log("SUBMIT BUTTON CLICKED")
+
+
+        //Get all information about the problem to send it to the backend. 
+        // Problem name and number
+        // Difficulty, 
+        //
+
+
+
+
+        let code_string = document.querySelector("div[role='presentation'][data-mprt='8']")
+
+        if (code_string){
+
+            // console.log(code_string)
+            
+        }
 
 
         // Check if the solution is accepted or rejected
         check_if_solution_is_accepted_or_rejeted()
         .then((obj) => {
             
-            console.log(obj)
+            // console.log(obj)
 
 
 
@@ -87,7 +119,7 @@ if (submit_btn){
         .catch((error) => {
 
 
-            console.log(error)
+            // console.log(error)
 
         }) //Do nothing after. Wait for the submit button to be clicked on again
 
